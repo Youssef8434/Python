854874c66a17eefab38e0107a6ad6a7e2fd182f5
Youssef8434 <146384920+Youssef8434@users.noreply.github.com>
2023-10-01 00:32:50 +0300

    Comments

---
+++ comments.py
@@ -0,0 +1,17 @@
+#This is a comment 
+print("Hello, world")
+
+print("Good Morning") #This is a comment
+
+#print("Hello world")
+print("check mate")
+
+#This is a comment
+#Just one line 
+print("Hello world")
+
+"""
+This is a comment 
+Just one line 
+print ("python dev")
+"""
