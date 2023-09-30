c5b811c987d41f54f6e9c1651b49628310f2abad
Youssef8434 <146384920+Youssef8434@users.noreply.github.com>
2023-10-01 01:02:45 +0300

    variables

---
+++ variables.py
@@ -0,0 +1,31 @@
+x = "python code"
+y = "Youssef"
+print(x)
+print(y) 
+
+x = 5 
+x = "Youssef"
+print (x)
+
+x = str (5)   # x will be '5'
+y = int (5)   # y will be 5
+z = float (5)   # z will be 5.0
+print(x)
+print(y)
+print(z)
+
+x = 5
+y = "Youssef"
+print(type(x))
+print(type(y))
+
+x = "Youssef"
+# is the same as
+x = 'Youssef'
+
+a = 5 
+A = "Youssef"
+print(a)
+print(A)
+
+
