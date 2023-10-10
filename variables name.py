2c6fa5788086c16ad5f468eeb5536539b12fd383
Youssef8434 <146384920+Youssef8434@users.noreply.github.com>
2023-10-10 20:45:21 +0300

    variables name

---
+++ variables name.py
@@ -0,0 +1,18 @@
+myvar = "Joe"
+my_var = "Joe"
+_my_var = "Joe"
+myVar = "Joe"
+MYVAR = "Joe"
+myvar2 = "Joe"
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+
+#this is a comment
+# Camel Case = myVariableName = "John"
+# Pascal Case = MyVariableName = "John"
+# Snake case = my_variable_name = "John"
