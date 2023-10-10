34412d6d17e49e4f5810892692b12c797bf25dd4
Youssef8434 <146384920+Youssef8434@users.noreply.github.com>
2023-10-10 20:54:23 +0300

    Assign Multiple Values

---
+++ assign multiple values.py
@@ -0,0 +1,18 @@
+x, y, z = "Orange", "Black", "Red"
+
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+
+print(x)
+print(y)
+print(z)
+
+games = ["gta", "valorant", "fortnite"]
+x, y, z = games
+
+print(x)
+print(y)
+print(z)
