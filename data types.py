d3a42732c0d99c4aa6bc8d99b43f46ff70de56c6
Youssef8434 <146384920+Youssef8434@users.noreply.github.com>
2023-10-11 20:05:51 +0300

    data types

---
+++ data types.py
@@ -0,0 +1,63 @@
+x = 5
+print(type(x))
+
+x = "Hello World"
+print(x)
+print(type(x)) 
+
+x = "Hello World"      #str 
+print(x)
+print(type(x)) 
+
+x = 20      #int
+print(x)
+print(type(x)) 
+
+x = 1j     #complex
+print(x)
+print(type(x)) 
+
+x = ["apple", "banana", "cherry"]     #list 
+print(x)
+print(type(x)) 
+
+x = ("apple", "banana", "cherry")       #tuple
+print(x)
+print(type(x)) 
+
+x = range(6)     #range
+print(x)
+print(type(x)) 
+
+x = {"name" : "John", "age" : 36}      #dict 
+print(x)
+print(type(x)) 
+
+x = {"apple", "banana", "cherry"}     #set
+print(x)
+print(type(x)) 
+
+x = frozenset({"apple", "banana", "cherry"})     #frozenset
+print(x)
+print(type(x)) 
+
+x = True     #bool 
+print(x)
+print(type(x)) 
+
+x = b"Hello"    #bytes
+print(x)
+print(type(x)) 
+
+x = bytearray(5)      #bytearray
+print(x)
+print(type(x)) 
+
+x = memoryview(bytes(5))     #memoryview
+print(x)
+print(type(x)) 
+
+x = None       #none 
+print(x)
+print(type(x)) 
+
