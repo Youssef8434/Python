1ed515c282875b99a421ae217d8d17a996f56d1b
Youssef8434 <146384920+Youssef8434@users.noreply.github.com>
2023-10-13 19:21:41 +0300

    modify strings

---
+++ modify strings.py
@@ -0,0 +1,15 @@
+a = "Hello, World!"   #UPPER CASE
+print(a.upper())
+
+a = "Hello, World!"   #lower case
+print(a.lower())
+
+a = " Hello, World! "     #returns "Hello, World!"
+print(a.strip())
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+a = "Hello, World!"
+b = a.split(",")
+print(b)
